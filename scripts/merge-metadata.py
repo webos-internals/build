14b2a674a5b656cad0f34ac5a63372a5dc0a1639
Rod Whitby <rod@whitby.id.au>
2009-08-18 17:21:37 +0930

    Only output Package data for packages that have Metadata entries

--- scripts/merge-metadata.py
+++ scripts/merge-metadata.py
@@ -14,19 +14,33 @@ for line in fileinput.input([sys.argv[1]]) :
     m = regexp.match(line)
     if (m):
         key = m.group(1)
+        metadata[key] = None
 
     regexp = re.compile('^Source: (.*)$')
     m = regexp.match(line)
     if (m):
         metadata[key] = m.group(1)
 
+packagedata = ""
+
 for line in fileinput.input([sys.argv[2]]) :
 
-    sys.stdout.write(line)
+    if line == "\n":
+        continue
+
+    packagedata += line
 
     regexp = re.compile('^Filename: (.*)$')
     m = regexp.match(line)
     if (m):
         key = m.group(1)
         if key in metadata:
-            print "Source: " + metadata[key]
+            packagedata += "Source: " + metadata[key] + "\n"
+
+    regexp = re.compile('^Description: (.*)$')
+    m = regexp.match(line)
+    if (m):
+        if key in metadata:
+            print packagedata
+            print
+        packagedata = ""
