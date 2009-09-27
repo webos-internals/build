a45424390893b6f59af054d6237105aa9190aca4
Eric <emoney_33@yahoo.com>
2009-09-27 00:59:28 -0400

    Added more patches from gitorious modifications repo. Bumped version

--- autopatch/common.mk
+++ autopatch/common.mk
@@ -3,7 +3,7 @@ APP_ID = org.webosinternals.patches.${NAME}
 HOMEPAGE = http://www.webos-internals.org/wiki/Portal:Patches_to_webOS
 MAINTAINER = WebOS Internals <support@webos-internals.org>
 DEPENDS = org.webosinternals.patch, org.webosinternals.diffstat
-MAIN_VERSION = 0.1.5
+MAIN_VERSION = 0.1.6
 
 ifdef PATCH_VERSION
 VERSION = ${MAIN_VERSION}-${PATCH_VERSION}
