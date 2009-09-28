3f7a21fd2c9c7bbf946856f1ded336f74dffdb76
Eric <emoney_33@yahoo.com>
2009-09-28 01:26:38 -0400

    Fixed patch name for substitution in postinst/prerm scripts bumped version.

--- autopatch/common.mk
+++ autopatch/common.mk
@@ -4,7 +4,7 @@ HOMEPAGE = http://www.webos-internals.org/wiki/Portal:Patches_to_webOS
 MAINTAINER = WebOS Internals <support@webos-internals.org>
 DEPENDS = org.webosinternals.patch, org.webosinternals.diffstat
 FEED = Autopatch
-MODS_VERSION = 0.1.11
+MODS_VERSION = 0.1.12
 
 ifndef VERSION
   ifdef MODS_EXTRA_VERSION
@@ -32,14 +32,14 @@ build/.built: build/.unpacked
 build/all/CONTROL/prerm: build/.unpacked
 	mkdir -p build/all/CONTROL
 	cp ../prerm build/all/CONTROL/prerm
-	sed -i -e 's|PATCH_NAME=|PATCH_NAME=${NAME}.patch|' build/all/CONTROL/prerm
+	sed -i -e 's|PATCH_NAME=|PATCH_NAME=$(shell basename ${PATCH})|' build/all/CONTROL/prerm
 	sed -i -e 's|APP_DIR=|APP_DIR=/var/usr/palm/applications/${APP_ID}|' build/all/CONTROL/prerm
 	chmod ugo+x $@
 
 build/all/CONTROL/postinst: build/.unpacked
 	mkdir -p build/all/CONTROL
 	cp ../postinst build/all/CONTROL/postinst
-	sed -i -e 's|PATCH_NAME=|PATCH_NAME=${NAME}.patch|' build/all/CONTROL/postinst
+	sed -i -e 's|PATCH_NAME=|PATCH_NAME=$(shell basename ${PATCH})|' build/all/CONTROL/postinst
 	sed -i -e 's|APP_DIR=|APP_DIR=/var/usr/palm/applications/${APP_ID}|' build/all/CONTROL/postinst
 	chmod ugo+x $@
 
