10c4766298b8b7ec5f0ae8fe2667f31b2204b4d0
Rod Whitby <rod@whitby.id.au>
2009-12-17 12:07:16 +1030

    Move webOS Doctor images out of the downloads directory, as they are not redisributable

--- support/patch-md5sums.mk
+++ support/patch-md5sums.mk
@@ -2,7 +2,7 @@
 md5sums: build/md5sums
 
 .PRECIOUS: build/md5sums
-build/md5sums: ${DL_DIR}/webosdoctorp100ewwsprint.jar
+build/md5sums: ${DOCTOR_DIR}/webosdoctorp100ewwsprint.jar
 	rm -rf build/md5sums
 	mkdir -p build
 	unzip -p $< resources/webOS.tar | \
@@ -10,7 +10,7 @@ build/md5sums: ${DL_DIR}/webosdoctorp100ewwsprint.jar
 	tar -C build -m -z -x -f - ./md5sums
 	touch $@
 
-${DL_DIR}/webosdoctorp100ewwsprint.jar :
-	mkdir -p ${DL_DIR}
+${DOCTOR_DIR}/webosdoctorp100ewwsprint.jar :
+	mkdir -p ${DOCTOR_DIR}
 	#curl -L -o $@ http://palm.cdnetworks.net/rom/pre_p100eww/webosdoctorp100ewwsprint.jar
 	curl -L -o $@ http://palm.cdnetworks.net/rom/ash994djslspam356z/s2x56ydt/webosdoctorp100ewwsprint.jar
