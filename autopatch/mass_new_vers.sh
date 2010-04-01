dd503cfd750d91e2903d17e8acb606ccfe334bc5
Rod Whitby <rod@whitby.id.au>
2010-04-01 12:47:59 +1030

    Patches for 1.4.1

---
+++ autopatch/mass_new_vers.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+LIST=`find . -name Makefile`
+echo > test.log
+for v in ${LIST} ; do
+  VERSIONS=$(awk '/^VERSIONS =/ {print}' $v)
+  if [ "$VERSIONS" ] ; then
+    SINPLU=VERSIONS
+  else
+    SINPLU=VERSION
+  fi
+  OLD_VERSION=$(awk '/^'$SINPLU' =/ {print}' $v)
+  OUTPUT_VERSION="$OLD_VERSION v1.4.1-1"
+  echo "sed -i.orig -e 's|$OLD_VERSION|$OUTPUT_VERSION|' $v" >> test.log
+done
