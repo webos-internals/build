f5c6093c3ae7532b05370b65a6fc9fbfaf5a5705
Rod Whitby <rod@whitby.id.au>
2010-04-11 12:39:47 +0930

    Change cross-compilation to use cs2007q3-51, cause that's what Palm uses for the rootfs build

--- support/cross-compile.mk
+++ support/cross-compile.mk
@@ -1,6 +1,6 @@
-CROSS_COMPILE_armv7 = $(shell cd ../.. ; pwd)/toolchain/cs08q1armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
-CROSS_COMPILE_armv6 = $(shell cd ../.. ; pwd)/toolchain/cs08q1armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
-CROSS_COMPILE_arm   = $(shell cd ../.. ; pwd)/toolchain/cs08q1armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
+CROSS_COMPILE_armv7 = $(shell cd ../.. ; pwd)/toolchain/cs07q3armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
+CROSS_COMPILE_armv6 = $(shell cd ../.. ; pwd)/toolchain/cs07q3armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
+CROSS_COMPILE_arm   = $(shell cd ../.. ; pwd)/toolchain/cs07q3armel/build/arm-2007q3/bin/arm-none-linux-gnueabi-
 CROSS_COMPILE_i686  = $(shell cd ../.. ; pwd)/toolchain/i686-unknown-linux-gnu/build/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-
 
 STAGING_DIR_armv7 = $(shell cd ../.. ; pwd)/staging/armv7
