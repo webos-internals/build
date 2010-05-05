c6a292113a0b150f9f938f5c9995a525bd27256e
Rod Whitby <rod@whitby.id.au>
2010-05-05 22:17:21 +0930

    Added headlessapp support to all kernels.  Updated uber-kernel in testing

--- support/kernel.mk
+++ support/kernel.mk
@@ -16,6 +16,8 @@ POSTINSTALLFLAGS = RestartDevice
 POSTUPDATEFLAGS  = RestartDevice
 POSTREMOVEFLAGS  = RestartDevice
 
+HEADLESSAPP_VERSION = 0.1.0
+
 KERNEL_DISCLAIMER = WebOS Internals provides this program as is without warranty of any kind, either expressed or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose.  The entire risk as to the quality and performance of this program is with you.  Should this program prove defective, you assume the cost of all necessary servicing, repair or correction.<br>\
 In no event will WebOS Internals or any other party be liable to you for damages, including any general, special, incidental or consequential damages arising out of the use or inability to use this program (including but not limited to loss of data or data being rendered inaccurate or losses sustained by you or third parties or a failure of this program to operate with any other programs).
 
@@ -69,6 +71,7 @@ include ../../support/download.mk
 
 include ../../support/doctors.mk
 
+include ../../support/headlessapp.mk
 
 .PHONY: unpack
 unpack: build/.unpacked-${VERSION}
@@ -76,15 +79,25 @@ unpack: build/.unpacked-${VERSION}
 .PHONY: build
 build: build/.built-${VERSION}
 
-build/.built-${VERSION}: build/arm.built-${VERSION}
+build/.built-${VERSION}: build/arm.built-${VERSION} ${DL_DIR}/headlessapp-${HEADLESSAPP_VERSION}.tar.gz
+	tar -C build/arm/usr/palm/applications/${APP_ID} -xvf ${DL_DIR}/headlessapp-${HEADLESSAPP_VERSION}.tar.gz
+	rm -rf build/arm/usr/palm/applications/${APP_ID}/.git
 	cp ../../support/kernel.png build/arm/usr/palm/applications/${APP_ID}/icon.png
 	echo "{" > build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "\"title\": \"${TITLE}\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "\"id\": \"${APP_ID}\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "\"version\": \"${WEBOS_VERSION}\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "\"vendor\": \"WebOS Internals\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
+	echo "\"type\": \"web\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
+	echo "\"main\": \"index.html\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "\"icon\": \"icon.png\"," >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
-	echo "\"type\": \"web\"" >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
+ifdef APPINFO_DESCRIPTION
+	echo '"message": "${APPINFO_DESCRIPTION}",' >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
+endif
+ifdef APPINFO_CHANGELOG
+	echo '"changeLog": ${APPINFO_CHANGELOG},' >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
+endif
+	echo "\"noWindow\": true" >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	echo "}" >> build/arm/usr/palm/applications/${APP_ID}/appinfo.json
 	touch $@
 
