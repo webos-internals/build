d2ef830b863bcc3b15c16eb85c84c191d9b13ec1
Jauder Ho <codecrumb+git@gmail.com>
2010-06-25 18:31:35 -0700

    omit-frame-pointer

--- support/cross-compile.mk
+++ support/cross-compile.mk
@@ -13,7 +13,7 @@ CONFIGURE_HOST_armv6 = arm-none-linux-gnueabi
 CONFIGURE_HOST_arm   = arm-none-linux-gnueabi
 CONFIGURE_HOST_i686  = i686-unknown-linux-gnu
 
-CFLAGS_armv7 = -O2 -fexpensive-optimizations -fexpensive-optimizations -frename-registers
-CFLAGS_armv6 = -O2 -fexpensive-optimizations -fexpensive-optimizations -frename-registers
-CFLAGS_arm   = -O2 -fexpensive-optimizations -fexpensive-optimizations -frename-registers
-CFLAGS_i686  = -O2 -fexpensive-optimizations -fexpensive-optimizations -frename-registers
+CFLAGS_armv7 = -O2 -fexpensive-optimizations -fomit-frame-pointer -frename-registers
+CFLAGS_armv6 = -O2 -fexpensive-optimizations -fomit-frame-pointer -frename-registers
+CFLAGS_arm   = -O2 -fexpensive-optimizations -fomit-frame-pointer -frename-registers
+CFLAGS_i686  = -O2 -fexpensive-optimizations -fomit-frame-pointer -frename-registers
