71525d9a637b763719fc413f7a8e6caf0d0c4ca9
Eric <emoney_33@yahoo.com>
2010-07-27 21:57:40 -0400

    Merge modules.mk into kernel.mk, adding support for building kernel packages along with modules packages. Create built kernel tarballs for each webos version and device for quicker building of kernels/modules

--- support/doctors.mk
+++ support/doctors.mk
@@ -14,6 +14,10 @@ ${DOCTOR_DIR}/webosdoctorp100ueu-wr-1.4.1.jar:
 	mkdir -p ${DOCTOR_DIR}
 	curl -L -o $@ http://palm.cdnetworks.net/rom/pre/p141r0d03312010/eudep141rod/webosdoctorp100ueu-wr.jar
 
+${DOCTOR_DIR}/webosdoctorp121ueu-wr-1.4.1.jar:
+	mkdir -p ${DOCTOR_DIR}
+	curl -L -o $@ http://palm.cdnetworks.net/rom/pixiplus/px141r0d04272010/wrep141rod/webosdoctorp121ueu-wr.jar
+
 ${DOCTOR_DIR}/webosdoctorp101ewwatt-1.4.2.jar:
 	mkdir -p ${DOCTOR_DIR}
 	curl -L -o $@ http://palm.cdnetworks.net/rom/preplus/p142r0d05162010/attp142rod/webosdoctorp101ewwatt.jar
@@ -25,3 +29,7 @@ ${DOCTOR_DIR}/webosdoctorp121ewwatt-1.4.3.jar:
 ${DOCTOR_DIR}/webosdoctorp100ueu-wr-1.4.5.jar:
 	mkdir -p ${DOCTOR_DIR}
 	curl -L -o $@ http://palm.cdnetworks.net/rom/pre/p145r0d06302010/eudep145rod/webosdoctorp100ueu-wr.jar
+
+${DOCTOR_DIR}/webosdoctorp121ewweu-wr-1.4.5.jar:
+	mkdir -p ${DOCTOR_DIR}
+	curl -L -o $@ http://palm.cdnetworks.net/rom/pixiplus/px145r0d06302010/wrep145rod/webosdoctorp121ewweu-wr.jar
