03da40aa279218c67ddbdef4229e1f5c99130a39
Rod Whitby <rod@whitby.id.au>
2011-07-17 11:10:00 +0930

    Initial version with upstart script and chroot scripts

---
+++ nonworking/ubuntu-natty-chroot/bin/chroot-wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CHROOT=/media/ext3fs/ubuntu-natty-chroot
+
+cp /etc/resolv.conf ${CHROOT}/etc/resolv.conf
+
+mount --bind /dev ${CHROOT}/dev
+mount -t devpts none ${CHROOT}/dev/pts
+mount -t proc none ${CHROOT}/proc
+mount --bind /sys ${CHROOT}/sys
+mount --bind /tmp ${CHROOT}/tmp
+
+chroot ${CHROOT} /bin/bash
+
+umount ${CHROOT}/tmp
+umount ${CHROOT}/sys
+umount ${CHROOT}/proc
+umount ${CHROOT}/dev/pts
+umount ${CHROOT}/dev
+
