415d358612430e47e0f4c3240e30b197c2ade0ac
Rod Whitby <rod@whitby.id.au>
2011-08-27 10:30:52 +0930

    Debian Chroot

---
+++ testing/debian-squeeze-chroot/bin/start-xterm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NAME=debian-squeeze-chroot
+
+APPID=org.webosinternals.${NAME}
+
+APPDIR=/media/cryptofs/apps/usr/palm/applications/${APPID}
+
+XTERM=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xterm/bin/xterm
+
+${XTERM} -display :0.0 -maximize -e "sh -x ${APPDIR}/bin/chroot-wrapper.sh" &
