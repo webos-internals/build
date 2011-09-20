70a9f3df911308e4a2c91f917c1adfda17d66803
Matthew Van Gent <scoutcamper@gmail.com>
2011-09-20 16:34:31 -0700

    Added XApps Shimm Signed-off-by: Matthew Van Gent <scoutcamper@gmail.com>

---
+++ alpha-apps/ubuntu-natty-chroot/usr/sbin/Xapps.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ACTION=$1
+APP_NAME=$2
+APP_INSTALL_LOG_DIR=/var/log/
+INSTALLER_LOG=xapps_installer.log
+ERROR_LOG=xapps_installer_error.log
+UPDATE_LOG=update.log
+DATE=date +%Y%m%d%H%M%S
+
+rm ${APP_INSTALL_LOG_DIR}${ERROR_LOG}
+
+case ${ACTION} in
+
+      update)
+             if ! `apt-get update > #{APP_INSTALL_LOG_DIR}${UPDATE_LOG}`
+             then 
+                 echo '${DATE} apt-get update failed' >> ${APP_INSTALL_LOG_DIR}${ERROR_LOG} 
+             fi
+             ;;
+
+      install)
+              if `apt-get install ${APP_NAME}  \
+              >> ${APP_INSTALL_LOG_DIR}${INSTALLER_LOG} 2>&1`
+              then
+                  echo '[${DATE}] Install Succeeded ${APP_NAME}' >> ${APP_INSTALL_LOG_DIR}${INSTALLER_LOG}
+             else 
+                  echo '[${DATE}] Installer Failed ${APP_NAME}' >> ${APP_INSTALL_LOG_DIR}${ERROR_LOG}
+             fi
+             ;;
+
+       remove)
+              if `apt-get remove ${APP_NAME}  \
+              >> ${APP_INSTALL_LOG_DIR}${INSTALLER_LOG} 2>&1`
+              then                  
+                   echo '[${DATE}] Remove  Succeeded ${APP_NAME}' >> ${APP_INSTALL_LOG_DIR}${INSTALLER_LOG}
+              else
+                    echo '[${DATE}] Remove  Failed ${APP_NAME}' >> ${APP_INSTALL_LOG_DIR}${ERROR_LOG}
+             fi
+             ;;
+
+esac
