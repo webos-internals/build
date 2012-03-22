5af1066801b208658eac712d8730ac3ae73b67c1
scoutcamper <scoutcamper@scoutcamper.dyndns.org>
2012-03-21 18:14:47 -0700

    Moved Ubuntu-Oneiric to alpha-apps Signed-off-by: scoutcamper <scoutcamper@scoutcamper.dyndns.org>

---
+++ alpha-apps/ubuntu-oneiric-chroot/bin/start-xterm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export DISPLAY=${1:-:0.0}
+
+NAME=ubuntu-oneiric-chroot
+
+APPID=org.webosinternals.${NAME}
+
+APPDIR=/media/cryptofs/apps/usr/palm/applications/${APPID}
+
+XTERM=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xterm/bin/xterm
+
+${XTERM} -display ${DISPLAY} -maximize -e "sh -x ${APPDIR}/bin/chroot-wrapper.sh" &
