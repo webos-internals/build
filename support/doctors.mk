af4986df2257e75dc3c31dca4d0cc7aaa544f6a2
Rod Whitby <rod@whitby.id.au>
2012-07-28 08:11:57 +0930

    Fixed the breakage by scoutcamper.

--- support/doctors.mk
+++ support/doctors.mk
@@ -1,5 +1,5 @@
 ${DOCTOR_DIR}/webosdoctorp100ueu-wr-1.4.5.jar:
-  mkdir -p ${DOCTOR_DIR}
+	mkdir -p ${DOCTOR_DIR}
 	curl -L -o $@ http://downloads.help.palm.com/webosdoctor/rom/pre/p145r0d06302010/eudep145rod/webosdoctorp100ueu-wr.jar
 
 ${DOCTOR_DIR}/webosdoctorp121ewweu-wr-1.4.5.jar:
